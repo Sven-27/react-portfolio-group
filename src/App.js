cc8c02443006fbc1089b03fac39cc00692b05012
TommmyKelly <tommy_kelly@icloud.com>
2020-11-15 18:40:12 +0000

    ADD: adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import ContactForm from './ContactForm'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div className="app">
-      <Router>
+
+      
       <Navbar />
-      </Router>
+      <Switch>
+      <Route exact path="/contact" component={ContactForm}/> 
+      </Switch>
    
     </div>
+    </Router>
   );
 }
 
