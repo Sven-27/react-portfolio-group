f2ae0c4a21cf3544b3150167e732a5b7ca8d9858
Sven Notermans <nootje27@outlook.com>
2020-11-16 23:00:09 +0100

    adjusted homepage

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home() {
         <div className="home">
             <div className="home__content">
             <img src="https://cdn.eso.org/images/thumb700x/airglow_ats_beletsky-CC.jpg" alt=""/>
-            <h1>My name is <span>Sven Notermans</span></h1>
+            <h1>I am <span>Sven Notermans</span></h1>
             </div>
         </div>
     )
