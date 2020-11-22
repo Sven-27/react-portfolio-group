c6498699f0916502f4569274501c58b81938e148
Joe <joedias@hotmail.com>
2020-11-22 12:51:18 -0500

    Push Skills component - 11/22/20

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Skills from './Skills';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="app">
       <Router>
       <Navbar />
+      
       </Router>
    
     </div>
