b44d752a749d3c93852a5de22c873a2e53773de1
Rami David <56394661+RDmaz@users.noreply.github.com>
2020-11-30 23:05:50 +0100

    first push of the about component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
-import Blog from './Blog'
-import Skills from "./Skills"
-import ContactForm from './ContactForm';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Blog from "./Blog";
+import Skills from "./Skills";
+import ContactForm from "./ContactForm";
+import About from "./About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 function App() {
-  return (
-    <Router>
-      <div className="app">
-    <Navbar />
+	return (
+		<Router>
+			<div className="app">
+				<Navbar />
 
-      <Switch>
-
-      <Route exact path="/" component={Home} />
-      <Route exact path="/contact" component={ContactForm}/> 
-      <Route exact path="/skills" component={Skills} />
-      <Route exact path="/blog" component={Blog} />
-      </Switch>
-
-   
-    </div>
-    </Router>
-  );
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/contact" component={ContactForm} />
+					<Route exact path="/skills" component={Skills} />
+					<Route exact path="/blog" component={Blog} />
+					<Route exact path="/About" component={About} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
