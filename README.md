ac48ece4c1623f98961f1400ea469d122da0d574
AJ Thomas <ajthomas12@msn.com>
2020-12-06 13:58:08 -0700

    New Team Portfolio Branch

---
+++ README.md
@@ -0,0 +1,12 @@
+Todo:
+
+Components:
+1 Navbar done
+2 Home page made by Sven
+3 Skills made by Joe
+4 Blog made by Don
+5 About made by Rami
+6 Contact made by Tommy
+7 Footer made by AJ
+8 Projects made by Ayesha
+9 Testimonials made by Punitha
---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import Footer from './Footer';
+import Blog from './Blog'
+import Skills from "./Skills"
+import ContactForm from './ContactForm';
+import About from './About';
+import Projects from './Projects';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <div className="app">
+
+        <Switch>
+
+          <Route exact path="/" >
+
+            <Navbar />
+            <Home />
+
+          </Route>
+
+          <Route exact path="/contact" >
+
+            <Navbar />
+            <ContactForm />
+            <Footer />
+
+          </Route>
+
+          <Route exact path="/skills" >
+
+            <Navbar />
+            <Skills />
+            <Footer />
+
+          </Route>
+
+          <Route exact path="/about" >
+
+            <Navbar />
+            <About />
+            <Footer />
+
+          </Route>
+
+          <Route exact path="/blog" >
+
+            <Navbar />
+            <Blog />
+            <Footer />
+
+          </Route>
+
+          <Route exact path="/projects" >
+
+            <Navbar />
+            <Projects />
+            <Footer />
+
+          </Route>
+
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
