0a13e4190c87758a1566eb45c349db5fdbe80d13
K.Niroshani <niroshini.07.skk@gmail.com>
2020-12-16 16:53:27 +0530

    ADD: cutomer img

--- src/Testimonial.js
+++ src/Testimonial.js
@@ -6,6 +6,10 @@ function Testimonial() {
     <div className="testimonial">
       <h1 className="testimonial__title">What do our customers say...</h1>
       <div className="testimonial__container">
+        <img
+          src="https://clementjames.org/wp-content/uploads/2019/09/avatar-1577909_960_720-1.png"
+          alt="Customer Image"
+        />
         <p>
           <span>“</span> Lorem ipsum dolor sit, amet consectetur adipisicing
           elit. Tempore neque officiis, praesentium natus qui facilis vitae
@@ -17,6 +21,10 @@ function Testimonial() {
         <h5>Sydney, Australia</h5>
       </div>
       <div className="testimonial__container">
+        <img
+          src="https://clementjames.org/wp-content/uploads/2019/09/avatar-1577909_960_720-1.png"
+          alt="Customer Image"
+        />
         <p>
           <span>“</span> Lorem ipsum dolor sit, amet consectetur adipisicing
           elit. Tempore neque officiis, praesentium natus qui facilis vitae
@@ -28,6 +36,10 @@ function Testimonial() {
         <h5>Los Angeles, USA</h5>
       </div>
       <div className="testimonial__container">
+        <img
+          src="https://clementjames.org/wp-content/uploads/2019/09/avatar-1577909_960_720-1.png"
+          alt="Customer Image"
+        />
         <p>
           <span>“</span> Lorem ipsum dolor sit, amet consectetur adipisicing
           elit. Tempore neque officiis, praesentium natus qui facilis vitae
