97f0224f0a602708537681f1fe9100dc46af38cf
K.Niroshani <niroshini.07.skk@gmail.com>
2020-12-16 16:42:25 +0530

    UPDATE: Testimonial.js

--- src/Testimonial.js
+++ src/Testimonial.js
@@ -6,37 +6,37 @@ function Testimonial() {
     <div className="testimonial">
       <h1 className="testimonial__title">What do our customers say...</h1>
       <div className="testimonial__container">
-      <p>
-        <span>"</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Tempore neque officiis, praesentium natus qui facilis vitae quasi
-        doloribus, delectus quia corporis animi atque? Iste veniam debitis sequi
-        voluptas fugit porro?
-      </p>
-      <hr />
-      <h3>Anna Jones</h3>
-      <h5>Sydney, Australia</h5>
+        <p>
+          <span>“</span> Lorem ipsum dolor sit, amet consectetur adipisicing
+          elit. Tempore neque officiis, praesentium natus qui facilis vitae
+          quasi doloribus, delectus quia corporis animi atque? Iste veniam
+          debitis sequi voluptas fugit porro?
+        </p>
+        <hr />
+        <h3>Anna Jones</h3>
+        <h5>Sydney, Australia</h5>
       </div>
       <div className="testimonial__container">
-      <p>
-        <span>"</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Tempore neque officiis, praesentium natus qui facilis vitae quasi
-        doloribus, delectus quia corporis animi atque? Iste veniam debitis sequi
-        voluptas fugit porro?
-      </p>
-      <hr />
-      <h3>Alex Damian</h3>
-      <h5>Los Angeles, USA</h5>
+        <p>
+          <span>“</span> Lorem ipsum dolor sit, amet consectetur adipisicing
+          elit. Tempore neque officiis, praesentium natus qui facilis vitae
+          quasi doloribus, delectus quia corporis animi atque? Iste veniam
+          debitis sequi voluptas fugit porro?
+        </p>
+        <hr />
+        <h3>Alex Damian</h3>
+        <h5>Los Angeles, USA</h5>
       </div>
       <div className="testimonial__container">
-      <p>
-        <span>"</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Tempore neque officiis, praesentium natus qui facilis vitae quasi
-        doloribus, delectus quia corporis animi atque? Iste veniam debitis sequi
-        voluptas fugit porro?
-      </p>
-      <hr />
-      <h3>John Cameron</h3>
-      <h5>Finsbury , London</h5>
+        <p>
+          <span>“</span> Lorem ipsum dolor sit, amet consectetur adipisicing
+          elit. Tempore neque officiis, praesentium natus qui facilis vitae
+          quasi doloribus, delectus quia corporis animi atque? Iste veniam
+          debitis sequi voluptas fugit porro?
+        </p>
+        <hr />
+        <h3>John Cameron</h3>
+        <h5>Finsbury , London</h5>
       </div>
     </div>
   );
