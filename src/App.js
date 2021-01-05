de3531b4c2b98819f32f06c7033f62e471e23a15
TommmyKelly <tommy_kelly@icloud.com>
2021-01-05 21:35:53 +0000

    add scroll efect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 import Home from "./Home";
-import Footer from './Footer'
+import Footer from "./Footer";
 import Skills from "./Skills";
 import ContactForm from "./ContactForm";
 import About from "./About";
-import Projects from './Projects'
-
+import Projects from "./Projects";
 
 function App() {
+  //setting refs to each componemts
+  let pageRef = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+  ];
+
+  //state for scroll active
+
+  const [scrollActive, setScrollActive] = useState();
+  //set state for main container
+  const foot = useRef();
+
+  // ignore -> const pageHomeRef = [React.createRef(), React.createRef()];
+
+  //scroll to element on click
+  const scrollToRef = (ref) => {
+    console.log(ref.current.offsetTop);
+    window.scrollTo({ top: ref.current.offsetTop, behavior: "smooth" });
+  };
+
+  const scrollToPane = (num) => scrollToRef(pageRef[num]);
+
+  //Scroll to bottom
+  const scrollToBottom = () => {
+    const scroll = foot.current.scrollHeight;
+
+    console.log(foot.current.getBoundingClientRect().top);
+
+    window.scrollTo(0, scroll);
+  };
+
+  //change active state on scroll
+
+  useEffect(() => {
+    document.addEventListener("scroll", isInViewport);
+    return () => {
+      document.removeEventListener("scroll", isInViewport);
+    };
+  }, []);
+
+  const isInViewport = () => {
+    //get how much pixels left to scrolling our ReactElement
+    const home = pageRef[0].current.getBoundingClientRect().top;
+    const about = pageRef[1].current.getBoundingClientRect().top;
+    const projects = pageRef[2].current.getBoundingClientRect().top;
+    const skills = pageRef[3].current.getBoundingClientRect().top;
+    const contact = pageRef[4].current.getBoundingClientRect().top;
+
+    //here we check if element top reference is on the top of viewport
+    /*
+     * If the value is positive then top of element is below the top of viewport
+     * If the value is zero then top of element is on the top of viewport
+     * If the value is negative then top of element is above the top of viewport
+     * */
+    if (home <= 0) {
+      console.log("Element is in view or above the viewport");
+      setScrollActive("home");
+    }
+    if (about <= 0) {
+      console.log("Element is in view or above the viewport");
+      setScrollActive("about");
+    }
+    if (projects <= 0) {
+      console.log("Element is in view or above the viewport");
+      setScrollActive("projects");
+    }
+    if (skills <= 0) {
+      console.log("Element is in view or above the viewport");
+      setScrollActive("skills");
+    }
+    if (contact <= 0) {
+      setScrollActive("contact");
+    }
+  };
   return (
-      <div className="app">
-        <div id="home"><Home /></div>
-        <div id="about"><About  /></div>
-        <div id="projects"><Projects  /></div>
-        <div id="skills"><Skills  /></div>
-        <div id="contact"><ContactForm  /></div>
-        <div id="footer"><Footer /></div>
+    <div ref={foot} className='app'>
+      <div ref={pageRef[0]}>
+        <Home
+          scrollToPane={scrollToPane}
+          scrollToBottom={scrollToBottom}
+          scrollActive={scrollActive}
+        />
       </div>
+      <div ref={pageRef[1]}>
+        <About />
+      </div>
+      <div ref={pageRef[2]}>
+        <Projects />
+      </div>
+
+      <div ref={pageRef[3]}>
+        <Skills />
+      </div>
+      <div ref={pageRef[4]}>
+        <ContactForm />
+      </div>
+      <div ref={pageRef[5]}>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
 export default App;
- 
--- src/Home.js
+++ src/Home.js
@@ -1,67 +1,183 @@
-import React, { useState, useRef, useEffect } from 'react'
-import './Home.css';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import React, { useState, useRef, useEffect } from "react";
+import "./Home.css";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
 // eslint-disable-next-line
 // import Typing from './Typing';
 
-//live share test  
+//live share test
 // Here is another comment.
 
-function Home() {
-    const [buttonState, setbuttonState] = useState(true)
-    const [activeState, setActiveState] = useState("home")
-    const homePage = useRef(null)
+function Home({ scrollToPane, scrollToBottom, scrollActive }) {
+  const [buttonState, setbuttonState] = useState(true);
+  const [activeState, setActiveState] = useState("home");
+  // const [homelink, setHomelink] = useState();
+  // const [aboutlink, setAboutlink] = useState();
+  // const [projectlink, setProjectlink] = useState();
+  // const [skillslink, setSkillslink] = useState();
+  // const [contactlink, setContactlink] = useState();
+  const homePage = useRef(null);
 
-         const hideMobileMenu = () => {
-             if (window.innerWidth < 672) {
-                 setbuttonState(true)
-             }
-            }
+  const hideMobileMenu = () => {
+    if (window.innerWidth < 672) {
+      setbuttonState(true);
+    }
+  };
 
-           useEffect(() => {
-                window.scrollTo({top: homePage.current.offsetTop, behavior: 'smooth'})
-            }, [])
+  function test(link, ref) {
+    scrollToPane(ref);
+    setActiveState(link);
+  }
 
-    return (
-        <div ref={homePage} className="home">
-        <header className="home__container">
-        <div className="home__home" id="div" >
-            <div className={buttonState ? "home__buttons hide" : "home__buttons"}> 
+  useEffect(() => {
+    window.scrollTo({
+      top: homePage.current.offsetTop,
+      behavior: "smooth",
+    });
+  }, []);
+
+  useEffect(() => {
+    setActiveState(scrollActive);
+  }, [scrollActive]);
+
+  return (
+    <div ref={homePage} className='home'>
+      <header className='home__container'>
+        <div className='home__home' id='div'>
+          <div className={buttonState ? "home__buttons hide" : "home__buttons"}>
             <ul onClick={hideMobileMenu}>
-                    <li onClick={() => setActiveState("home")}><a className={activeState === 'home' ? 'home__home active' : 'home__home'} href='#home'>Home</a></li>
-                    <li onClick={() => setActiveState("about")}><a className={activeState === 'about' ? 'home__home active' : 'home__home'} href='#about'  >About</a></li>
-                    <li onClick={() => setActiveState("projects")}><a className={activeState === 'projects' ? 'home__home active' : 'home__home'} href='#projects' >Projects</a></li>
-                    <li onClick={() => setActiveState("skills")}><a className={activeState === 'skills' ? 'home__home active' : 'home__home'} href='#skills'  >Skills</a></li>
-                    <li onClick={() => setActiveState("contact")}><a className={activeState === 'contact' ? 'home__home active' : 'home__home'} href='#contact'>Contact</a></li>
-                    <li onClick={() => setActiveState("footer")}><a className={activeState === 'footer' ? 'home__home active' : 'home__home'} href='#footer' >Footer</a></li>
-                 </ul>
-                 </div>
+              <li onClick={() => test("home", 0)}>
+                <a
+                  className={
+                    activeState === "home" ? "home__home active" : "home__home"
+                  }
+                  href='#home'
+                >
+                  Home
+                </a>
+              </li>
+              <li onClick={() => test("about", 1)}>
+                <a
+                  className={
+                    activeState === "about" ? "home__home active" : "home__home"
+                  }
+                  href='#about'
+                >
+                  About
+                </a>
+              </li>
+              <li onClick={() => test("projects", 2)}>
+                <a
+                  className={
+                    activeState === "projects"
+                      ? "home__home active"
+                      : "home__home"
+                  }
+                  href='#projects'
+                >
+                  Projects
+                </a>
+              </li>
+              <li onClick={() => test("skills", 3)}>
+                <a
+                  className={
+                    activeState === "skills"
+                      ? "home__home active"
+                      : "home__home"
+                  }
+                  href='#skills'
+                >
+                  Skills
+                </a>
+              </li>
+              <li onClick={() => test("contact", 4)}>
+                <a
+                  className={
+                    activeState === "contact"
+                      ? "home__home active"
+                      : "home__home"
+                  }
+                  href='#contact'
+                >
+                  Contact
+                </a>
+              </li>
+              <li onClick={scrollToBottom}>
+                <a
+                  className={
+                    activeState === "footer"
+                      ? "home__home active"
+                      : "home__home"
+                  }
+                  href='#footer'
+                >
+                  Footer
+                </a>
+              </li>
+            </ul>
+          </div>
 
-            <div className="mobile__button" onClick={() => setbuttonState(!buttonState)}>
-                {buttonState ? <i class="fa fa-bars" aria-hidden="true"></i> : <i class="fa fa-times" aria-hidden="true"></i>}
-            </div>
-         </div> 
-        </header>
-            <div className="home__content">
-                <div className="home__imageProfile"></div>
-                <h1>I am <span>Sven Notermans</span></h1>
-                <p>Fullstack Developer</p>
-                <div className="home__icons">
-                    <a href="https://github.com/Sven-27/react-portfolio-group" target="_blank" rel="noreferrer"><GitHubIcon /></a>
-                    <a href="https://www.linkedin.com/in/sven-notermans" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
-                    <a href="https://www.facebook.com/Svengebon" target="_blank" rel="noreferrer"><FacebookIcon /></a>
-                    <a href="https://www.instagram.com/svennie_27/" target="_blank" rel="noreferrer"><InstagramIcon /></a>
-                    <a href="https://twitter.com/Sven__27" target="_blank" rel="noreferrer"><TwitterIcon /></a>
-                </div>
-            
-            </div>
+          <div
+            className='mobile__button'
+            onClick={() => setbuttonState(!buttonState)}
+          >
+            {buttonState ? (
+              <i class='fa fa-bars' aria-hidden='true'></i>
+            ) : (
+              <i class='fa fa-times' aria-hidden='true'></i>
+            )}
+          </div>
         </div>
-    )
+      </header>
+      <div className='home__content'>
+        <div className='home__imageProfile'></div>
+        <h1>
+          I am <span>Sven Notermans</span>
+        </h1>
+        <p>Fullstack Developer</p>
+        <div className='home__icons'>
+          <a
+            href='https://github.com/Sven-27/react-portfolio-group'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <GitHubIcon />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/sven-notermans'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            href='https://www.facebook.com/Svengebon'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <FacebookIcon />
+          </a>
+          <a
+            href='https://www.instagram.com/svennie_27/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <InstagramIcon />
+          </a>
+          <a
+            href='https://twitter.com/Sven__27'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <TwitterIcon />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
-
+export default Home;
