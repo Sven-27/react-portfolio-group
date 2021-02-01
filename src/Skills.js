2d905f25ea40eb9847980e8615507c889b2bd811
Sven Notermans <sven.notermans@live.nl>
2021-02-01 20:56:17 +0100

    worked on contactform and skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './Skills.css'
 // import SkillBar from 'react-skillbars';
 
 function Skills() {
+  useEffect(() => {
+    document.addEventListener("scroll", toggleClass)
+    return () => {
+      document.removeEventListener("scroll", toggleClass)
+    }
+  }, [])
+
+  const [bar1, setBar1] = useState("")
+  const [bar2, setBar2] = useState("")
+  const [bar3, setBar3] = useState("")
+  const [bar4, setBar4] = useState("")
+  const [bar5, setBar5] = useState("")
+  const [bar6, setBar6] = useState("")
+
+  const toggleClass = () => {
+    const home = skills.current.getBoundingClientRect().top
+    const homeHeight = skills.current.getBoundingClientRect().height
+
+    if(home <= 250 && home >= -homeHeight){
+      setBar1("skills__bar bar-1")
+      setBar2("skills__bar bar-2")
+      setBar3("skills__bar bar-3")
+      setBar4("skills__bar bar-4")
+      setBar5("skills__bar bar-5")
+      setBar6("skills__bar bar-6")
+    }
+  }
+
+  const skills = useRef()
 
   return (
-    <div className="skills">
+    <div className="skills" ref={skills}>
     <div className="skills__imageContainer">
     <i class="fab fa-html5"></i>
     <i class="fab fa-css3-alt"></i>
@@ -18,27 +47,27 @@ function Skills() {
     
         <h4>HTML5</h4>
         <div className="skills__progress">
-          <div className="skills__bar bar-1" style={{"width": '95%', "animation-name": "bar-1", "animation-duration": "3s", "animation-timing-function": "ease-in-out"}}><span>95%</span></div>
+          <div className={bar1} style={{"width": '95%'}}><span>95%</span></div>
         </div>
         <h4>CSS3</h4>
         <div className="skills__progress">
-          <div className="skills__bar bar-2" style={{"width": '95%', "animation-name": "bar-2", "animation-duration": "3s", "animation-timing-function": "ease-in-out"}}><span>95%</span></div>
+          <div className={bar2} style={{"width": '95%'}}><span>95%</span></div>
         </div>
         <h4>Javascript</h4>
         <div className="skills__progress">
-          <div className="skills__bar bar-3" style={{"width": '75%', "animation-name": "bar-3", "animation-duration": "3s", "animation-timing-function": "ease-in-out"}}><span>75%</span></div>
+          <div className={bar3} style={{"width": '75%'}}><span>75%</span></div>
         </div>
         <h4>Bootstrap</h4>
         <div className="skills__progress">
-          <div className="skills__bar bar-4" style={{"width": '80%', "animation-name": "bar-4", "animation-duration": "3s", "animation-timing-function": "ease-in-out"}}><span>80%</span></div>
+          <div className={bar4} style={{"width": '80%'}}><span>80%</span></div>
         </div>
         <h4>React</h4>
         <div className="skills__progress">
-          <div className="skills__bar bar-5" style={{"width": '70%', "animation-name": "bar-5", "animation-duration": "3s", "animation-timing-function": "ease-in-out"}}><span>70%</span></div>
+          <div className={bar5} style={{"width": '70%'}}><span>70%</span></div>
         </div>
         <h4>Node.js</h4>
         <div className="skills__progress">
-          <div className="skills__bar b ar-6" style={{"width": '70%', "animation-name": "bar-6", "animation-duration": "3s", "animation-timing-function": "ease-in-out"}}><span>70%</span></div>
+          <div className={bar6} style={{"width": '70%'}}><span>70%</span></div>
         </div>
       </div>
         
